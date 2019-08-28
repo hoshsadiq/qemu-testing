2d546dff20ca7e2e84df114452fe33d7c0b27362
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2019-08-28 23:19:24 +0100

    testing

--- mkimage.sh
+++ mkimage.sh
@@ -2,11 +2,11 @@
 
 set -euxo pipefail
 
-cd /tmp/aports-3.10.1/scripts
+cd /workdir/aports-3.10.1/scripts
 
 ./mkimage.sh \
     --arch aarch64 \
-    --outdir /workdir/build \
+    --outdir ./build \
     --profile rpi_custom \
     --tag v3.10.1 \
     --repository http://dl-cdn.alpinelinux.org/alpine/v3.10/main \
