85590e33420469a519f1157c0e939acae244501d
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2019-09-25 20:56:32 +0100

    restructure

--- build/scripts/006-kernel.sh
+++ build/scripts/006-kernel.sh
@@ -3,6 +3,6 @@
 set -eux
 
 # todo does cmdline.txt support comments?
-cat /build/kernel/cmdline.txt | grep -v '^#' > /boot/cmdline.txt
-cp /build/kernel/config.txt /boot/config.txt
-cp /build/kernel/fstab.txt /etc/fstab
+cat /build/assets/kernel/cmdline.txt | grep -v '^#' > /boot/cmdline.txt
+cp /build/assets/kernel/config.txt /boot/config.txt
+cp /build/assets/kernel/fstab.txt /etc/fstab
